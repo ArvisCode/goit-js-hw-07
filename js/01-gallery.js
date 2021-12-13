6df20e3b79a310dd6f1e8fec05e0a4233df5a0c2
Volodymyr Horbatiuk <vladimir.a.gorbatyuk@gmail.com>
2021-12-13 23:08:04 +0200

    test release

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRefs = document.querySelector(".gallery");
+galleryRefs.addEventListener("click", onShowLargeImage);
+
+(function createMarkup() {
+  const itemMarkup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>
+    `;
+    })
+    .join("");
+  galleryRefs.insertAdjacentHTML("beforeend", itemMarkup);
+})();
+
+function onShowLargeImage(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+	let largeImageSrc = event.target.dataset.source;
+
+  const modal = basicLightbox.create(
+    `<img src="${largeImageSrc}" width="800" height="600">`
+  );
+  modal.show();
+
+  if (modal.visible()) {
+    window.addEventListener("keydown", onPressKeyESC);
+  }
+
+  function onPressKeyESC(event) {
+    if (event.code === "Escape") {
+      modal.close();
+      window.removeEventListener("keydown", onPressKeyESC);
+    }
+  }
+}
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRefs = document.querySelector(".gallery");
+
+console.log(galleryRefs);
+
+(function createMarkup() {
+  const itemMarkup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>
+    `;
+    })
+    .join("");
+  galleryRefs.insertAdjacentHTML("beforeend", itemMarkup);
+})();
+
+const lightbox = new SimpleLightbox(".gallery a", {
+	overlay: false,
+	captions: true,
+	captionsData: "alt",
+	closeText: "Close",
+	disableRightClick: true,
+  
+});
 
 console.log(galleryItems);
